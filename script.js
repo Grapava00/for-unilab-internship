3363d4c59437991604e84fc788e6b64bf0f1bd16
Grapava00 <Grapava00@gmail.com>
2022-06-17 04:13:26 +0400

    end project

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const burgers = document.getElementsByClassName('header__burger--icon')
 const headerContainer = document.getElementsByClassName('header-container')
 const navbars=document.getElementsByClassName('navbar')
+const listContainer=document.getElementById('list-container')
 
 for(const burger of burgers){
     burger.addEventListener('click', ()=>{
@@ -20,3 +21,14 @@ for(const burger of burgers){
 }
 
 
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(json => console.log(json.map(result =>  listContainer.innerHTML+=
+    `
+        <div class="list-block">
+          <span class="block">block ${result.id}</span>
+          <h2 key="${result.id}" class="title">${result.title}</h2>
+          <p class="body">${result.body}</p>
+        </div>
+    `
+    )))
